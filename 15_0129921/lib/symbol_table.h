72dc58e4754e0198b22b6b19c3f876d02a6da14a
Igor Figueira Pinheiro da Silva <igorfgds@gmail.com>
2021-09-07 10:47:56 -0300

    Add function args to symbol table and AST - missing header commits

--- 15_0129921/lib/symbol_table.h
+++ 15_0129921/lib/symbol_table.h
@@ -10,6 +10,7 @@
 typedef struct symbol_table_entry symbol_table_entry_t;
 typedef struct inner_scope inner_scope_t;
 typedef struct symbol_table symbol_table_t;
+typedef struct func_param func_param_t;
 
 struct symbol_table_entry {
   char* id;
@@ -20,6 +21,13 @@ struct symbol_table_entry {
   };
   symbol_table_entry_t* prev;
   symbol_table_entry_t* next;
+};
+
+struct func_param {
+  char* id;
+  char* data_type;
+  func_param_t* prev;
+  func_param_t* next;
 };
 
 struct symbol_table {
@@ -40,9 +48,12 @@ void add_symbol_table_entry(symbol_table_t*, char*, char*);
 void add_symbol_table_to_inner_scope(symbol_table_t*, symbol_table_t*);
 void print_symbol_table(symbol_table_t*, int);
 void free_symbol_table(symbol_table_t*);
+void save_func_param(char*, char*);
+void add_params_to_symbol_table(symbol_table_t*);
 
 // Initialize symbol table
-symbol_table_t *symbol_table;
-symbol_table_t *current_symbol_table;
+symbol_table_t* symbol_table;
+symbol_table_t* current_symbol_table;
+func_param_t* func_params_list;
 
 #endif
